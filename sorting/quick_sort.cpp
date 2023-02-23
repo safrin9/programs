52138b63aa4a8f7e8eb863547866b3486d7f3fbe
safrin9 <safrin36771@gmail.com>
2023-02-23 20:00:52 +0600

    sorting

---
+++ sorting/quick_sort.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> quick_sort(vector<int>&a)
+{
+    if(a.size()<=1)
+    {
+    return a;
+    }
+    int pivot=a.size()-1;
+    vector<int>b;
+    vector<int>c;
+    for(int i=0;i<a.size();i++)
+    {
+    if(i==pivot)
+    continue;
+    if(a[i]<=a[pivot])
+
+        b.push_back(a[i]);
+    else
+    c.push_back(a[i]);
+
+    }
+
+    vector<int>sorted_b=quick_sort(b);
+    vector<int>sorted_c=quick_sort(c);
+    vector<int>sorted_a;
+    //conquer
+    for(int i=0;i<sorted_b.size();i++)
+
+        sorted_a.push_back(sorted_b[i]);
+
+    sorted_a.push_back(a[pivot]);
+
+     for(int i=0;i<sorted_c.size();i++)
+
+        sorted_a.push_back(sorted_c[i]);
+
+    return sorted_a;
+
+
+
+
+
+}
+int main()
+{
+    vector<int>a={3,7,8,9,4,1,9};
+    vector<int>sorted_a=quick_sort(a);
+    for(int i=sorted_a.size()-1;i>=0;i--)
+    {
+        cout<<sorted_a[i]<<" ";
+    }
+    return 0;
+
+}
+
